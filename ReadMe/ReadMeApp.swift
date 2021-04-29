c09d0beff1f76014ec8638d1e8722ce1217dee56
pavan kovurru <pava1n.k@gmail.com>
2021-04-29 10:57:57 -0500

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by pavan Kovurru on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
